e0faf14e38b89c405b0a7fabb4ee25b8aafdb3bc
= <=>
2023-05-14 22:54:28 +0300

    test made for base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,11 @@
+""" this is a unit test module for base_model module """
+import unittest
+from models.base_model import BaseModel
+
+
+class BaseModelTest(unittest.TestCase):
+    """ test class for BaseModel class"""
+    def test_types(self):
+        """ tests if id is string of not """
+        bb = BaseModel()
+        self.assertEqual(type(bb.id), str)
