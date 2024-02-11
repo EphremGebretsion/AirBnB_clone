8406ad7d361a3171b78fd50c2d4996318529e29a
EphremGebretsion <ephhrem@gmail.com>
2024-02-12 02:53:02 +0300

    console with create, all, show, destroy

--- console.py
+++ console.py
@@ -101,8 +101,8 @@ class HBNBCommand(cmd.Cmd):
 
     def help_create(self):
         """prints the help info of create command"""
-        info = "Creates the specified class\n "
-        "prints the id then saves it to json the file\n"
+        info = ''.join(["Creates the specified class",
+                        " prints the id then saves it to json the file\n"])
         use = "Usage: create <class name>\n"
         print(info, use)
 
@@ -132,9 +132,134 @@ class HBNBCommand(cmd.Cmd):
 
     def help_show(self):
         """prints help for show command"""
-        info = "searches for an instance with specified id and class name\n"
-        "then prints its string format"
+        info = ''.join(["searches for an instance with specified id and",
+                        " class name then prints its string format\n"])
         use = "Usage: show <class name> <instance id>\n"
+        print(info, use)
+
+    def do_destroy(self, line):
+        """deletes an instance and saves the change to the json file"""
+        class_name = None
+        instance_id = None
+        args = []
+        if line:
+            args = split_str(line)
+        if args:
+            class_name = args[0]
+        if len(args) >= 2:
+            instance_id = args[1]
+        if not class_name:
+            print("** class name missing **")
+        elif class_name != "BaseModel":
+            print("** class doesn't exist **")
+        elif not instance_id:
+            print("** instance id missing **")
+        else:
+            my_key = class_name + "." + instance_id
+            if my_key in storage.all().keys():
+                del storage.all()[my_key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def help_destroy(self):
+        """prints help for destroy command"""
+        info = ''.join(["Deletes the given class instance by id ",
+                        "and saves to the json file\n"])
+        usage = "Usage: destroy <class name> <id>\n"
+        print(info, usage)
+
+    def do_all(self, line):
+        """prints all string representation of class instances"""
+        class_name = None
+        args = []
+        if line:
+            args = split_str(line)
+        if args:
+            class_name = args[0]
+        if not class_name:
+            my_list = []
+            for key, value in storage.all().items():
+                my_list.append(value.__str__())
+            print(my_list)
+        elif class_name != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            my_list = []
+            for key, value in storage.all().items():
+                if class_name == value.__class__.__name__:
+                    my_list.append(value.__str__())
+            print(my_list)
+
+    def help_all(self):
+        """prints help for all command"""
+        info = ''.join(["prints all instances of a specified class or",
+                        " all instances that are stored in the json file\n"])
+        usage = "Usage: all <class name>"
+        print(info, usage)
+
+    def do_update(self, line):
+        """updates the instance based on class name and id"""
+        args = []
+        class_name = None
+        instance_id = None
+        attr = None
+        value = None
+        leng = 0
+        if line:
+            args = split_str(line)
+        if args:
+            class_name = args[0]
+            leng = len(args)
+        if leng >= 2:
+            instance_id = args[1]
+        if leng >= 3:
+            attr = args[2]
+        if leng >= 4:
+            value = args[3]
+        if not class_name:
+            print("** class name missing **")
+        elif class_name != "BaseModel":
+            print("** class doesn't exist **")
+        elif not instance_id:
+            print("** instance id missing **")
+        elif class_name + "." + instance_id not in storage.all().keys():
+            print("** no instance found **")
+        elif not attr:
+            print("** attribute name missing **")
+        elif not value:
+            print("** value missing **")
+        else:
+            my_value = value
+            flag = True
+            while flag:
+                if '.' in my_value:
+                    try:
+                        my_value = float(my_value)
+                        break
+                    except ValueError:
+                        pass
+                elif 'True' == my_value:
+                    my_value = True
+                    break
+                elif 'False' == my_value:
+                    my_value = False
+                    break
+                try:
+                    my_value = int(my_value)
+                    break
+                except ValueError:
+                    pass
+                flag = False
+            key = class_name + '.' + instance_id
+            obj = storage.all()[key]
+            setattr(obj, attr, my_value)
+            obj.save()
+
+    def help_update(self):
+        """prints help for update command"""
+        info = "updates existing instance with a gived id\n"
+        use = "Usage: update <class name> <instance id> <attr> <value>\n"
         print(info, use)
 
 
