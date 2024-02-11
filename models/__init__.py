333328c86daa007c9a712e695c77f77909ffe6a4
EphremGebretsion <ephhrem@gmail.com>
2024-02-11 10:45:46 +0300

    requirment fix

--- models/__init__.py
+++ models/__init__.py
@@ -4,8 +4,8 @@ init file to store and reloading from the json storage file
 this will excute any time the module is imported
 so it adds the json file content to the storage instance
 """
-
-
 from models.engine import file_storage
+
+
 storage = file_storage.FileStorage()
 storage.reload()
