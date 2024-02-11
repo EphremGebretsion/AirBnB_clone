048ef2a0c740f7c0a19e91b79ac60a2fc6e0cea7
EphremGebretsion <ephhrem@gmail.com>
2024-02-11 13:48:35 +0300

    req fix...

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -21,10 +21,11 @@ class TestFileStorage(TestCase):
             os.remove("file.json")
         except:
             pass
-        d = FileStorage._FileStorage__objects
         my_keys = []
         for key in storage.all().keys():
             my_keys.append(key)
+        for key in my_keys:
+            del FileStorage._FileStorage__objects[key]
 
     def test_attr(self):
         """
